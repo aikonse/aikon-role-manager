61529e271f0f58505ad5ae62ca75bf68a5a59519
Andreas Andersson <andreas.1.andersson@gavle.se>
2025-01-24 01:04:52 +0100

    added add role handler

--- src/OptionsPage/Tab/RolesTab.php
+++ src/OptionsPage/Tab/RolesTab.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Aikon\RoleManager\OptionsPage\Tab;
 
 use Aikon\RoleManager\OptionsPage\Interfaces\TabInterface;
+use Aikon\RoleManager\OptionsPage\Traits\HandlesAction;
+use Aikon\RoleManager\OptionsPage\Traits\HandlesNotice;
 use Aikon\RoleManager\OptionsPage\Traits\HasTemplate;
 use Aikon\RoleManager\OptionsPage\Traits\HasTitleAnSlug;
 use Aikon\RoleManager\RoleManager;
@@ -13,6 +15,8 @@ class RolesTab implements TabInterface
 {
     use HasTitleAnSlug;
     use HasTemplate;
+    use HandlesAction;
+    use HandlesNotice;
 
     public function __construct(
         private RoleManager $manager
@@ -23,7 +27,46 @@ class RolesTab implements TabInterface
 
     public function handle(): void
     {
+        $this->middleware(function ($request): void {
+            if (!current_user_can('manage_options')) {
+                throw new \Exception('You do not have permission to manage roles and capabilities');
+            }
+        });
 
+        $this->post_action('action', 'add_role', [$this, 'handle_add_role']);
+
+    }
+
+    /**
+     * Add a role
+     *
+     * @param array<string, string> $request
+     * @return void
+     */
+    public function handle_add_role(array $request): void
+    {
+        $role = $this->manager->validate_role_name($request['role'] ?? '');
+        $slug = $this->manager->validate_role_slug($request['slug'] ?? '');
+
+        if (!$role) {
+            $this->add_notice(__('Role name is required', 'aikon-role-manager'), 'error');
+        }
+
+        if (!$slug) {
+            $this->add_notice(__('Role slug is required', 'aikon-role-manager'), 'error');
+        }
+
+        if (!$role || !$slug) {
+            return;
+        }
+
+        if (isset($this->manager->current_roles()[$slug])) {
+            $this->add_notice(__('Role already exists', 'aikon-role-manager'), 'error');
+            return;
+        }
+
+        $this->manager->add_role($slug, $role);
+        $this->add_notice(__('Role added', 'aikon-role-manager'), 'success');
     }
 
     public function render(): void
---
+++ src/OptionsPage/Traits/HandlesNotice.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Aikon\RoleManager\OptionsPage\Traits;
+
+trait HandlesNotice
+{
+    /**
+     * Add a notice
+     *
+     * @param string $message The message to show
+     * @param string $type The type of notice
+     * @return void
+     */
+    public function add_notice(string $message, string $type = 'success'): void
+    {
+        $type = in_array($type, [
+            'info',
+            'error',
+            'success',
+            'warning'
+        ]) ? $type : 'success';
+
+        add_action('admin_notices', function () use ($message, $type): void {
+            printf('<div class="notice notice-%s"><p>%s</p></div>', $type, $message);
+        });
+    }
+}
