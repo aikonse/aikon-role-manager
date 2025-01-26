175c3d27d555a03c6ccb1f33db33fca6bf78fa81
Andreas Andersson <andreas.1.andersson@gavle.se>
2025-01-26 13:20:37 +0100

    Add role update and delete functionality to RolesTab

--- src/OptionsPage/Tabs/RolesTab.php
+++ src/OptionsPage/Tabs/RolesTab.php
@@ -35,7 +35,8 @@ class RolesTab implements TabInterface
         });
 
         $this->post_action('action', 'add_role', [$this, 'handle_add_role']);
-
+        $this->post_action('action', 'update_role', [$this, 'handle_update_role']);
+        $this->get_action('action', 'delete_role', [$this, 'handle_delete_role']);
     }
 
     /**
@@ -46,18 +47,20 @@ class RolesTab implements TabInterface
      */
     public function handle_add_role(array $request): void
     {
-        $role = $this->manager->validate_role_name($request['role'] ?? '');
+        $name = $this->manager->validate_role_name($request['name'] ?? '');
         $slug = $this->manager->validate_role_slug($request['slug'] ?? '');
 
-        if (!$role) {
-            $this->add_notice(__('Role name is required', 'aikon-role-manager'), 'error');
+        if (!$name) {
+            $this->add_notice(__('Role name is required', 'aikon-role-manager'), 'warning');
+            $this->add_error('name', __('Role name is required', 'aikon-role-manager'));
         }
 
         if (!$slug) {
-            $this->add_notice(__('Role slug is required', 'aikon-role-manager'), 'error');
+            $this->add_notice(__('Role slug is required', 'aikon-role-manager'), 'warning');
+            $this->add_error('slug', __('Role slug is required', 'aikon-role-manager'));
         }
 
-        if (!$role || !$slug) {
+        if (!$name || !$slug) {
             return;
         }
 
@@ -66,8 +69,81 @@ class RolesTab implements TabInterface
             return;
         }
 
-        $this->manager->add_role($slug, $role);
-        $this->add_notice(__('Role added', 'aikon-role-manager'), 'success');
+        $this->manager->add_role( $slug,  $name,);
+        wp_redirect(url_parser(['tab' => $this->slug]));
+        exit;
+    }
+
+    /**
+     * Update a role
+     *
+     * @param array<string, string> $request
+     * @return void
+     */
+    public function handle_update_role(array $request): void
+    {
+        $updating = $this->manager->validate_role_slug($request['edit_role'] ?? '');
+        $role = $this->manager->validate_role_name($request['name'] ?? '');
+        $slug = $this->manager->validate_role_slug($request['slug'] ?? '');
+
+        // Trying to update a role that does not exist
+        if (
+            !$updating ||
+            !$this->manager->role_exists($updating)
+        ) {
+            $this->add_notice(__('Role does not exist', 'aikon-role-manager'), 'error');
+            return;
+        }
+
+        if (!$role) {
+            $this->add_notice(__('Role name is required', 'aikon-role-manager'), 'warning');
+            $this->add_error('name', __('Role name is required', 'aikon-role-manager'));
+        }
+
+        if (!$slug) {
+            $this->add_notice(__('Role slug is required', 'aikon-role-manager'), 'warning');
+            $this->add_error('slug', __('Role slug is required', 'aikon-role-manager'));
+        }
+
+        if (!$role || !$slug) {
+            return;
+        }
+
+        if (
+            $updating !== $slug &&
+            $this->manager->role_exists($slug)
+        ) {
+            $this->add_notice(__('Role already exists', 'aikon-role-manager'), 'error');
+            return;
+        }
+
+        $this->manager->update_role($updating, $slug, $role);
+        $this->add_notice(__('Role updated', 'aikon-role-manager'), 'success');
+    }
+
+    private function handle_delete_role($request)
+    {
+        $role_slug = $this->manager->validate_role_slug($request['delete_role'] ?? '');
+
+
+        if (
+            !$role_slug ||
+            !$this->manager->role_exists($role_slug)
+        ) {
+            $this->add_notice(__('Role does not exist', 'aikon-role-manager'), 'warning');
+            return;
+        }
+
+        if ($this->manager->is_default_role($role_slug)) {
+            $this->add_notice(__('You cannot delete a default role', 'aikon-role-manager'), 'error');
+            return;
+        }
+
+        $this->manager->remove_role($role_slug);
+        $this->add_notice(__('Role deleted', 'aikon-role-manager'), 'success');
+
+        wp_redirect(url_parser([], ['delete_role', 'action']));
+        exit;
     }
 
     public function render(): void
